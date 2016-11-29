31799b9c162fe102bcda0639e859bd6a5727d779
mehanix <nixikunn@gmail.com>
2016-11-30 01:21:44 +0200

    Add files via upload

--- LoginFormTest/LoginFormTest/Form1.cs
+++ LoginFormTest/LoginFormTest/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 //1. import System.Data.SqlClient
 using System.Data.SqlClient;
+using System.Security.Cryptography;
 
 namespace LoginFormTest
 {
@@ -27,7 +28,11 @@ namespace LoginFormTest
             /*the @"" thing is a Connection String.*/
             SqlConnection sqlcon = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\Nix\Documents\Visual Studio 2015\Projects\LoginFormTest\LoginFormTest\DB\loginDB.mdf; Integrated Security = True; Connect Timeout = 30");
             //3.Make a query. The query is what we're going to ask the DB to do, pretty much.
-            string query = "Select * from [Table] Where username= '" + textBox1.Text + "' and password = '" + textBox2.Text + "'";
+
+            //Encryption time!!
+            //search for username and get the PW string
+
+            string query = "Select * from [Table] Where username= '" + textBox1.Text + "'";
             //4. Create an object which will send the query through our connection to the sql server
             SqlDataAdapter sda = new SqlDataAdapter(query,sqlcon);
             //5. Create a dataTable in which we'll put the login information that matches
@@ -35,15 +40,42 @@ namespace LoginFormTest
             //6.Fill the table with whichever entry in the DB fits the criteria of:having the username put in the textBox1.Text
             //and password from textBox2.Text
             sda.Fill(dtbl);
-            //7.If there is 1 element (a match has been found), allow access
+            //7.If there is 1 element, check for password matching :)
             if(dtbl.Rows.Count == 1)
             {
-                Form2 frm2 = new Form2();
-                this.Hide();
-                frm2.ShowDialog();
-                Close();
+                //get the saved string
+                string savedPasswordHash = dtbl.Rows[0][1].ToString();
+                //turn it into bytes
+                byte[] hashBytes = Convert.FromBase64String(savedPasswordHash);
+                //take the salt out of the string
+                byte[] salt = new byte[16];
+                Array.Copy(hashBytes, 0, salt, 0, 16);
+                //hash the user inputted PW with the salt
+                var pbkdf2 = new Rfc2898DeriveBytes(textBox2.Text, salt, 10000);
+                //put the damn thing in a byte vector.. instead of a string. why? why is this necessary?
+                //who am i to judge cryptography standards i guess
+                byte[] hash = pbkdf2.GetBytes(20);
+                //oh, this is why
+                //compare results! letter by letter!
+                //starting from 17 cause 0-16 are the salt
+                int ok = 1;
+                for (int i = 0; i < 20; i++)
+                    if (hashBytes[i + 16] != hash[i])
+                        ok = 0;
+                if (ok == 1)
+                {
+                    Form2 frm2 = new Form2();
+                    this.Hide();
+                    frm2.ShowDialog();
+                    Close();
+                }
+                else
+                {
+                    //if wrong password, show
+                    MessageBox.Show("Incorrect username or password!");
+                }
             }
-            //If incorrect, don't allow access
+            //If no match for username, show
             else
             {
                 MessageBox.Show("Incorrect username or password!");
@@ -79,6 +111,30 @@ namespace LoginFormTest
                     //write the query
                     string commString = "INSERT INTO [Table](username, password) VALUES (@val1, @val2)";
                     string constring = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\Users\Nix\Documents\Visual Studio 2015\Projects\LoginFormTest\LoginFormTest\DB\loginDB.mdf; Integrated Security = True; Connect Timeout = 30";
+
+
+
+
+                    //hashing and stuff
+                    //make a new byte array
+                    byte[] salt;
+                    /*generate salt*/
+                    new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+                    /*hash and salt it using PBKDF2*/
+                    var pbkdf2 = new Rfc2898DeriveBytes(textBox2.Text, salt, 10000);
+                    //place the string in the byte array (thats waht getbytes does)
+                    byte[] hash = pbkdf2.GetBytes(20);
+                    //make new byte array where to store the hashed password+salt
+                    //why 36? cause 20 are for the hash and 16 for the salt
+                    byte[] hashBytes = new byte[36];
+                    //place the god damn things where they belong
+                    Array.Copy(salt, 0, hashBytes, 0, 16);
+                    Array.Copy(hash, 0, hashBytes, 16, 20);
+
+                    //now, convert our fancy byte array to a string so i can put it in the db
+                    string savedPasswordHash = Convert.ToBase64String(hashBytes);
+
+
                     //connect to db
                     using (SqlConnection conn = new SqlConnection(constring))
                     {
@@ -91,7 +147,7 @@ namespace LoginFormTest
                             //where the sanitizing happens
                             //santizing prevents adding malitious pieces of code into the query strings
                             comm.Parameters.AddWithValue("@val1", textBox1.Text);
-                            comm.Parameters.AddWithValue("@val2", textBox2.Text);
+                            comm.Parameters.AddWithValue("@val2", savedPasswordHash);
                             //open the connection
                             conn.Open();
                             //execute the query we just wrote
